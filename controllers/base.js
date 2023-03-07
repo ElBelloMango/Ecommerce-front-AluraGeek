c72a40c47f176ab7e75a3721fb2e98ae92528932
Miguel Angel Gallego Zuleta <miguel.gallego@upb.edu.co>
2023-03-07 10:51:23 -0500

    Tudo banner listo

---
+++ controllers/base.js
@@ -0,0 +1,5 @@
+import '../styles/blocks/navbar.css'
+import '../styles/elements/button.css'
+import '../styles/elements/input.css'
+import '../styles/blocks/footer.css'
+
---
+++ controllers/login.js
@@ -0,0 +1,1 @@
+import '../styles/blocks/login.css'
