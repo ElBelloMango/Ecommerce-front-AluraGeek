4bd590b22a9661c84323f64c4fa07294b4d0bb0f
Miguel Angel Gallego Zuleta <miguel.gallego@upb.edu.co>
2023-03-09 15:24:23 -0500

    Proyecto finalizado

--- controllers/base.js
+++ controllers/base.js
@@ -3,3 +3,8 @@ import '../styles/elements/button.css'
 import '../styles/elements/input.css'
 import '../styles/blocks/footer.css'
 
+const contactForm = document.querySelector(".footer__form");
+contactForm.addEventListener("submit",(event)=>{
+    event.preventDefault();
+    window.location.href = "mailto:magozuleta@gmail.com"
+})
