ff0bc86d931bf7263a5e7586fb803cb498d247e4
Miguel Angel Gallego Zuleta <miguel.gallego@upb.edu.co>
2023-03-10 09:51:41 -0500

    upload to github pages

---
+++ docs/assets/productos-91cb1fa4.js
@@ -0,0 +1,1 @@
+import"./base-5371dab3.js";import{l as r}from"./product-service-33b11f10.js";import{b as t}from"./products-181ed323.js";const a=document.querySelector("[data-galery]");r().then(o=>{t(a,o)});
