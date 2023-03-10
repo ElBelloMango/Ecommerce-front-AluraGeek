f91ea24794fb043e2d39ea19a2e41e992ef06d59
Miguel Angel Gallego Zuleta <miguel.gallego@upb.edu.co>
2023-03-10 08:00:53 -0500

    visualización en pages

--- controllers/login.js
+++ controllers/login.js
@@ -1,1 +1,8 @@
 import '../styles/blocks/login.css'
+
+const login = document.querySelector("[data-login]");
+
+login.addEventListener("submit",(event)=>{
+    event.preventDefault();
+    window.location = "./tudo.html"
+})
