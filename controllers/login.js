89062d7476e14e7766bcedcdf919e02ab9eec7de
Miguel Angel Gallego Zuleta <miguel.gallego@upb.edu.co>
2023-03-10 10:38:15 -0500

    final fixes

--- controllers/login.js
+++ controllers/login.js
@@ -1,1 +1,7 @@
 import '../styles/blocks/login.css'
+const form = document.querySelector("[data-login-form]");
+
+form.addEventListener("submit",(e)=>{
+    e.preventDefault();
+    window.location = "tudo.html"
+})
