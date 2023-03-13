07da0ef4a5ddfd21e01955edc641d383724196c9
Miguel Angel Gallego Zuleta <miguel.gallego@upb.edu.co>
2023-03-13 15:23:17 -0500

    Funcionalidad de busqueda

--- controllers/allproducts.js
+++ controllers/allproducts.js
@@ -2,7 +2,19 @@ import { listProductos } from '../services/product-service.js'
 import { addToAllGalery } from './products.js';
 
 const galery = document.querySelector("[data-galery]");
-
+const parametros = new URL(window.location).searchParams.get("search");
 listProductos().then((data) => {
-    addToAllGalery(galery,data)
+    if (!parametros) {
+        addToAllGalery(galery, data);
+    }
+    else {
+        const content = `
+            <h3 class="galery__title">Todas las coincidencias de: ${parametros.toLocaleUpperCase()}</h3>
+            <span class="galery__seemore"><a href="./productos.html">Ver Todo 🡢</a></span>`
+        const header = document.querySelector(".galery__header");
+        header.innerHTML = content;
+        const regex = new RegExp(parametros, 'i');
+        const filter = data.filter((producto) => regex.test(producto.nombre));
+        addToAllGalery(galery, filter);
+    }
 })
--- controllers/base.js
+++ controllers/base.js
@@ -3,8 +3,42 @@ import '../styles/elements/button.css'
 import '../styles/elements/input.css'
 import '../styles/blocks/footer.css'
 
+
+const searchForm = document.querySelector("[data-search-form]");
+const searchInput = document.querySelector("[data-search-input]");
+const searchButton = document.querySelector("[data-search-button]");
+const logo = document.querySelector(".navbar__logo");
+
 const contactForm = document.querySelector(".footer__form");
-contactForm.addEventListener("submit",(event)=>{
+contactForm.addEventListener("submit", (event) => {
     event.preventDefault();
     window.location.href = "mailto:magozuleta@gmail.com"
 })
+
+searchButton.addEventListener("click", (e) => {
+    var x = window.matchMedia("(max-width: 700px)");
+    e.preventDefault();
+    if (x.matches){
+        window.location = "./search.html"
+    }
+    if (!x.matches && !searchInput.validity.valueMissing) {
+        searchInput.setCustomValidity("");
+        window.location = `./productos.html?search=${searchInput.value}`
+    }
+    else {
+        searchInput.setCustomValidity("Por favor ingrese un valor");
+        searchInput.reportValidity();
+    }
+})
+searchForm.addEventListener("submit",(e)=>{
+    e.preventDefault();
+    if (!searchInput.validity.valueMissing) {
+        searchInput.setCustomValidity("");
+        window.location = `./productos.html?search=${searchInput.value}`
+    }
+    else {
+        searchInput.setCustomValidity("Por favor ingrese un valor");
+        searchInput.reportValidity();
+    }
+})
+logo.addEventListener("click",()=>window.location="./tudo.html")
