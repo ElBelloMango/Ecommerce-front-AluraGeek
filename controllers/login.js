56ede61f13c29a3cf17112c2e83ed2c1dfce23cd
Miguel Angel Gallego Zuleta <miguel.gallego@upb.edu.co>
2023-03-15 11:54:48 -0500

    funcionalidad login implementada

--- controllers/login.js
+++ controllers/login.js
@@ -1,7 +1,29 @@
 import '../styles/blocks/login.css'
-const form = document.querySelector("[data-login-form]");
+import { createUser, getUser } from '../services/user-service.js';
+import { SHA256 } from 'crypto-js';
 
-form.addEventListener("submit",(e)=>{
+const form = document.querySelector("[data-login-form]");
+const auth = document.querySelector("[data-auth]");
+
+function errorAutenticacion(){
+    auth.classList.remove("form__auth--hidden");
+}
+
+form.addEventListener("submit", (e) => {
     e.preventDefault();
-    window.location = "tudo.html"
+    const email = form.children[0].value;
+    const password = form.children[1].value;
+    const hashpass = SHA256(password).toString();
+    getUser(email)
+        .then((user) => {
+            if (user["password-hash"] == hashpass) {
+                window.location = "./tudo.html"
+            }
+            else errorAutenticacion();
+        }).catch((e) => {
+            if (e.codigo == 404) {
+                errorAutenticacion();
+            }
+            else console.error("Error en el servidor" + e);
+        });
 })
